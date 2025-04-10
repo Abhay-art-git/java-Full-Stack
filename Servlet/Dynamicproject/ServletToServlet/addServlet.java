2eae354a3edae4bf3ad082116b9c259fed9f783e
Abhay Mahto <abhaymahto0533@gmail.com>
2025-04-10 12:30:20 +0530

    Create addServlet.java

---
+++ Servlet/Dynamicproject/ServletToServlet/addServlet.java
@@ -0,0 +1,28 @@
+package com.demoapp;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class addServlet extends HttpServlet
+{
+
+	 public void doGet(HttpServletRequest req , HttpServletResponse res) throws IOException, ServletException {
+		 int i = Integer.parseInt(req.getParameter("num1"));
+		 int j = Integer.parseInt(req.getParameter("num2"));
+		 
+		 int k = i+j;
+		
+		 RequestDispatcher rd = req.getRequestDispatcher("sq");
+		 rd.forward(req,res);
+
+	 }
+}
